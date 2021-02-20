fe2dfe06c0939c58939e840ae2a8c521e1d16681
Steven Baty <baty_steven@yahoo.com>
2021-02-20 06:01:18 -0700

    add to server.js

--- server.js
+++ server.js
@@ -2,3 +2,14 @@ const express = require('express');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+const routes = require('./routes');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.use('/', routes);
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
