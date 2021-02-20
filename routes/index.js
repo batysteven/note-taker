e893e00c603915aaae7a4841dcc594370358beed
Steven Baty <baty_steven@yahoo.com>
2021-02-20 05:57:31 -0700

    routes for notes.html and index.html working with insomia

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 const path = require('path');
 const router = require('express').Router();
+const { notes } = require('../db/db.json');
 
 router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/notes.html'));
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
 
 router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/index.html'));
+    res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 module.exports = router;
